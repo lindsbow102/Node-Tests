16e087dba9c16f3b40691e9862ff60b2a2748970
Lindsey Bowen <lindsbow102@yahoo.com>
2018-08-19 10:14:42 -0700

    Using the assertion library 'expect'

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,53 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     const res = utils.add(3,15);
+    expect(res)
+        .toBeA('number')
+        .toBe(18);
     
-    if (res !== 18) {
-        throw new Error(`Expected 18, but got ${res}.`)
-    }
+    // if (res !== 18) {
+    //     throw new Error(`Expected 18, but got ${res}.`)
+    // }
 });
 
 it('should return the square of a number', () => {
     const res = utils.square(9);
 
-    if(res !== 81) {
-        throw new Error(`Expected 81, but got ${res}.`);
-    }
+    expect(res)
+        .toBeA('number')
+        .toBe(81);
 });
+
+// it('should expect some values', () => {
+//     //expect(12).toNotBe(11); 
+    
+//     //toBe and notToBe does not work with arrays or objects but DOES work for numbers/strings/booleans
+
+//     //expect({ name: 'Lindsey' }).toEqual({ name: 'Lindsey' });
+//     //expect([2,3,4,5]).toInclude(5);  expect([2,3,4]).toExclude(1);
+
+//     expect({
+//         name: 'Lindsey',
+//         age: 38,
+//         location: 'Gilbert'
+//     }).toInclude({ location: 'Gilbert' });
+
+// });
+
+it('should verify first and last names are set', () => {
+    const user = {
+        age: 38,
+        location: 'Gilbert'
+    };
+    const res = utils.setName(user, 'Lindsey Bowen');
+    expect(user).toEqual(res);
+    expect(res).toInclude({
+        firstName: 'Lindsey',
+        lastName: 'Bowen'
+    })
+});
+
+// nodemon --exec 'npm test' (use double quotes for windows)
